69de01d6dbe46dd195482e8832c8b475940b0315
Lee Johnson <lee@givengain.ch>
2014-10-16 09:20:02 +0200

    fix typo in TryCatch example - missing $e

--- lib/Business/GoCardless.pm
+++ lib/Business/GoCardless.pm
@@ -111,7 +111,7 @@ appropriate error catching code (TryCatch in the below example):
             }
         }
     }
-    catch ( Business::GoCardless::Exception ) {
+    catch ( Business::GoCardless::Exception $e ) {
         # error specific to Business::GoCardless
         ...
         say $e->message;  # error message
